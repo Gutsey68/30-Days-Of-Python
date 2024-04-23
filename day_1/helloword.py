6b9ec4e3e587a8561168b2a4a522adc18ef97bf6
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-04-23 11:31:16 +0200

    Day 1

---
+++ day_1/helloword.py
@@ -0,0 +1,51 @@
+# check the python version 
+
+import math
+import sys
+print(sys.version)
+
+# operations
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# strings
+
+print('Gauthier')
+print('Seyzeriat')
+print('France')
+print('I am enjoying 30 days of python')
+
+# data types 
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneth','Python','Finland']))
+print(type('Gauthier'))
+print(type('Seyzeriat'))
+print(type('france'))
+
+# Example data types
+print("Integer example:", 8)  # integer
+print("Float example:", 7.4)  # float
+print("Complex number example:", 2 + 4j)  # complex
+print("String example:", 'hello world')  # string
+print("Boolean example:", True)  # boolean
+print("List example:", [1, 5, 3])  # list
+print("Tuple example:", ('push', 'pull', 'legs'))  # tuple
+print("Set example:", {1, 3, 7, 9})  # set
+print("Dictionary example:", {'name': 'gauthier', 'age': 27, 'location': 'alsace'})  # dictionary
+
+# Euclidean distance between (2, 3) and (10, 8)
+
+# d(p,q)^2 = (q1-p1)^2 + (q2-p2)^2
+
+distance = math.sqrt(((10 - 2)**2) + ((8 - 3)**2))
+print("Euclidean distance between (2, 3) and (10, 8) is:", distance)
---
+++ helloworld.py
@@ -0,0 +1,45 @@
+# this is a comment
+# this is also a comment
+
+"""this is a super 
+comment"""
+
+'Python looks cool'
+
+[1,2,5,6] # this is a list of numbers
+['banana', 'orange', 'mango'] # this is a list of fruits
+['banana', 10 , False, 9.81] # different data types
+
+{
+'first_name': 'gauthier',
+'last_name' : 'seyzeriat--meyer',
+'age': 27,
+'skills':['javascript', 'react', 'php']
+} # python dictionary
+
+('earth' , 'jupiter' , 'neptune') # a tuple
+
+{ 2, 4, 3, 5 } # a set
+{ 3.14, 9.81, 2.7 } # order is not important in set
+
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex number
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+# write: python --version in the command line 
